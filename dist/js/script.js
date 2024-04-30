818ba19267d4a9ae455928231032c8a191e0a90c
Noriyuki Mizuno <inc.sinka@protonmail.com>
2024-04-30 22:57:20 +0900

    Initial-setting

---
+++ dist/js/script.js
@@ -0,0 +1,48 @@
+
+jQuery(function ($) {
+
+  // スムーススクロール (絶対パスのリンク先が現在のページであった場合でも作動。ヘッダーの高さ考慮。)
+  $(document).on('click', 'a[href*="#"]', function () {
+    let time = 400;
+    let header = $('header').innerHeight();
+    let target = $(this.hash);
+    if (!target.length) return;
+    let targetY = target.offset().top - header;
+    $('html,body').animate({ scrollTop: targetY }, time, 'swing');
+    return false;
+  });
+});
+
+$('.js-hamburger').click(function () {
+  $(this).toggleClass('open');
+  if( $(this).hasClass('open') ) {
+    $('.js-drawer').fadeIn();
+  } else {
+    $('.js-drawer').fadeOut();
+  }
+});
+
+$(window).on('load resize', function(){
+  var wid = $(window).width();
+  if (wid >= 768) {
+    $('.js-drawer').css('display','block');
+  } else {
+    $('.js-drawer').css('display','none');
+  }
+});
+
+
+const swiper = new Swiper('.swiper-container', {
+  loop: true,
+  speed: 3000,
+  // loopedSlides: 2,
+  slidesPerView:  "auto",
+  spaceBetween: 24,
+  centeredSlides: true,
+  loopAdditionalSlides: 2,
+  // allowTouchMove: false,
+  autoplay: {
+      delay: 1000,
+      disableOnInteraction: false
+  },
+});
